10c2637136aa4ab9f8452b9ace4a3966cfa36875
Spencer Pequegnat <spencer.peq@gmail.com>
2022-08-03 14:14:41 -0500

    Property binding and interpolation syntax

--- pw/src/app/app.component.ts
+++ pw/src/app/app.component.ts
@@ -3,10 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  password = '';
+
   onButtonClick() {
-    console.log('Button was clicked')
+    this.password = 'MY PASSWORD'
   }
 }
